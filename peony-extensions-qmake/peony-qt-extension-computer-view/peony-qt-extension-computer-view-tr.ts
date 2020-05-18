a79c7d2fd81469517b04c8a8392e94bc8c2d9891
Aylinux <cihanalk@gmail.com>
2020-05-18 14:03:13 +0300

    Create peony-qt-extension-computer-view-tr.ts

---
+++ peony-extensions-qmake/peony-qt-extension-computer-view/peony-qt-extension-computer-view-tr.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr" sourcelanguage="en_GB">
+<context>
+    <name>ComputerItemDelegate</name>
+    <message>
+        <location filename="computer-view/computer-item-delegate.cpp" line="122"/>
+        <source>You should mount volume first</source>
+        <translation>Önce bölümü bağlamalısınız</translation>
+    </message>
+</context>
+<context>
+    <name>ComputerNetworkItem</name>
+    <message>
+        <location filename="computer-view/computer-network-item.cpp" line="45"/>
+        <source>Network Neighborhood</source>
+        <translation>Ağ Komşuları</translation>
+    </message>
+</context>
+<context>
+    <name>ComputerRemoteVolumeItem</name>
+    <message>
+        <location filename="computer-view/computer-remote-volume-item.cpp" line="47"/>
+        <source>Remote</source>
+        <translation>Uzak</translation>
+    </message>
+</context>
+<context>
+    <name>ComputerVolumeItem</name>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="32"/>
+        <source>Volume</source>
+        <translation>Bölüm</translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="40"/>
+        <source>File System</source>
+        <translation>Dosya Sistemi</translation>
+    </message>
+</context>
+<context>
+    <name>Peony::ComputerViewContainer</name>
+    <message>
+        <location filename="computer-view-container.cpp" line="65"/>
+        <source>Connect a server</source>
+        <translation>Servere bağlan</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="68"/>
+        <source>sftp://, etc...</source>
+        <translation>sftp://, gibi...</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="77"/>
+        <source>Unmount</source>
+        <translation>Bağlantıyı kes</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="83"/>
+        <location filename="computer-view-container.cpp" line="97"/>
+        <source>Property</source>
+        <translation>Özellik</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="85"/>
+        <source>You have to mount this volume first</source>
+        <translation>Önce bu bölümü bağlamalısınız</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="peony-computer-view-plugin.h" line="42"/>
+        <source>Computer View</source>
+        <translation>Bilgisayar Görünümü</translation>
+    </message>
+    <message>
+        <location filename="peony-computer-view-plugin.h" line="43"/>
+        <source>Show drives, network and personal directories.</source>
+        <translation>Sürücüleri, ağı ve kişisel dizinleri göster.</translation>
+    </message>
+</context>
+</TS>
