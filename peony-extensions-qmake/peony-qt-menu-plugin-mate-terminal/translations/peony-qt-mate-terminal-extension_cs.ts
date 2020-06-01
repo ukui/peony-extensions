b1f6a1fa37082c988fcbe9ae65f86d14164002f1
p-bo <pavel.borecki@gmail.com>
2020-06-01 20:57:29 +0200

    Create peony-qt-mate-terminal-extension_cs.ts
    Add Czech translation

---
+++ peony-extensions-qmake/peony-qt-menu-plugin-mate-terminal/translations/peony-qt-mate-terminal-extension_cs.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>Peony::MateTerminalMenuPlugin</name>
+    <message>
+        <location filename="../mate-terminal-menu-plugin.cpp" line="116"/>
+        <location filename="../mate-terminal-menu-plugin.cpp" line="126"/>
+        <source>Open Directory in Terminal</source>
+        <translation>Otevřít složku v Terminálu</translation>
+    </message>
+    <message>
+        <location filename="../mate-terminal-menu-plugin.h" line="43"/>
+        <source>Peony-Qt Mate Terminal Menu Extension</source>
+        <translation>Rozšíření do Peony-Qt pro nabídku Mate Terminal</translation>
+    </message>
+    <message>
+        <location filename="../mate-terminal-menu-plugin.h" line="44"/>
+        <source>Open Terminal with menu.</source>
+        <translationOtevřít Terminál z nabídky.</translation>
+    </message>
+</context>
+</TS>
