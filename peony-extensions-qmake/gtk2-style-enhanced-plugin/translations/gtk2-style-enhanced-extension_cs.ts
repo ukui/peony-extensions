2f27c2f7544d0dc80ea1c071444c5c73bc59057d
p-bo <pavel.borecki@gmail.com>
2020-06-01 20:36:04 +0200

    Create gtk2-style-enhanced-extension_cs.ts
    Add Czech translation

---
+++ peony-extensions-qmake/gtk2-style-enhanced-plugin/translations/gtk2-style-enhanced-extension_cs.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>Peony::Gtk2StyleEnhancedPlugin</name>
+    <message>
+        <location filename="../gtk2-style-enhanced-plugin.h" line="24"/>
+        <source>Gtk theme enhanced extension</source>
+        <translation>Rozšíření s vylepšeným Gtk motivem vzhledu</translation>
+    </message>
+    <message>
+        <location filename="../gtk2-style-enhanced-plugin.h" line="25"/>
+        <source>Improve the gtk-themed controls&apos; style and painting</source>
+        <translation>Vylepšit vzhled (styl a grafiku) ovládacích prvků s gtk motivem vzhledu</translation>
+    </message>
+</context>
+</TS>
