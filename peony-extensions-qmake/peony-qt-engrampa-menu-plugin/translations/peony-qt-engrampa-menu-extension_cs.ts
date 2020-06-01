15bcc6a6806d6f58c7a8a41aa6809a87b364e0b1
p-bo <pavel.borecki@gmail.com>
2020-06-01 20:45:24 +0200

    Create peony-qt-engrampa-menu-extension_cs.ts
    Add Czech localization.

---
+++ peony-extensions-qmake/peony-qt-engrampa-menu-plugin/translations/peony-qt-engrampa-menu-extension_cs.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>Peony::EngrampaMenuPlugin</name>
+    <message>
+        <location filename="../engrampa-menu-plugin.cpp" line="60"/>
+        <source>compress...</source>
+        <translation>zkomprimovat…</translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.cpp" line="75"/>
+        <source>uncompress to current path</source>
+        <translation>rozbalit do stávajícího umístění</translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.cpp" line="85"/>
+        <source>uncompress to specific path...</source>
+        <translation>rozbalit do zadaného umístění…</translation>
+    </message>
+    <message>
+        <source>uncompress to specific path</source>
+        <translation type="vanished">rozbalit do zadaného umístění</translation>
+    </message>
+    <message>
+        <source>Peony-Qt KArchive Menu Extension</source>
+        <translation type="vanished">Rozšíření do Peony-Qt pro nabídku KArchive.</translation>
+    </message>
+    <message>
+        <source>KArchive Menu Extension.</source>
+        <translation type="vanished">Rozšíření s nabídkou KArchive.</translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.h" line="40"/>
+        <source>Peony-Qt engrampa Extension</source>
+        <translation>Rozšíření do Peony-Qt pro engrampa</translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.h" line="41"/>
+        <source>engrampa Menu Extension.</source>
+        <translation>Rozšíření s nabídkou engrampa.</translation>
+    </message>
+</context>
+</TS>
