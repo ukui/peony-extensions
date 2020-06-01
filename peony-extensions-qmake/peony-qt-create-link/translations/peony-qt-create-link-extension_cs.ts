34d07cf77149bb1fd67a5358f267530529488c6f
p-bo <pavel.borecki@gmail.com>
2020-06-01 20:39:50 +0200

    Create peony-qt-create-link-extension_cs.ts
    Add Czech localization

---
+++ peony-extensions-qmake/peony-qt-create-link/translations/peony-qt-create-link-extension_cs.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>Peony::CreateLinkMenuPlugin</name>
+    <message>
+        <location filename="../create-link-menu-plugin.cpp" line="20"/>
+        <source>Create Link to Desktop</source>
+        <translation>Vytvořit odkaz na Ploše</translation>
+    </message>
+    <message>
+        <location filename="../create-link-menu-plugin.cpp" line="30"/>
+        <source>Create Link to...</source>
+        <translation>Vytvořit odkaz na…</translation>
+    </message>
+    <message>
+        <location filename="../create-link-menu-plugin.cpp" line="33"/>
+        <source>Choose a Directory to Create Link</source>
+        <translation>Zvolte složku, na kterou vytvořit odkaz</translation>
+    </message>
+    <message>
+        <location filename="../create-link-menu-plugin.h" line="20"/>
+        <source>Peony-Qt Create Link Extension</source>
+        <translation>Rozšíření do Peony-Qt pro vytváření odkazů</translation>
+    </message>
+    <message>
+        <location filename="../create-link-menu-plugin.h" line="21"/>
+        <source>Create Link Menu Extension.</source>
+        <translation>Rozšíření s nabídkou pro vytvoření odkazu.</translation>
+    </message>
+</context>
+</TS>
