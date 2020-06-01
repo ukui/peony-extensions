20bda67bf473b609035165d79cfad6dd8aa3f785
p-bo <pavel.borecki@gmail.com>
2020-06-01 20:30:46 +0200

    Create peony-qt-share-extension_cs.ts
    Add Czech translation

---
+++ peony-extensions-cmake/peony-qt-share/translations/peony-qt-share-extension_cs.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>NetUsershareHelper</name>
+    <message>
+        <source>Peony-Qt-Share-Extension</source>
+        <translation type="vanished">Peony-Qt-Share-Extension</translation>
+    </message>
+</context>
+<context>
+    <name>Peony::SharePropertiesPagePlugin</name>
+    <message>
+        <location filename="../share-properties-page-plugin.h" line="26"/>
+        <source>Peony Qt Share Extension</source>
+        <translation>Rozšíření do Peony Qt pro správu sdílení</translation>
+    </message>
+    <message>
+        <location filename="../share-properties-page-plugin.h" line="27"/>
+        <source>Allow user share folders</source>
+        <translation>Umožnit uživateli sdílet složku</translation>
+    </message>
+</context>
+<context>
+    <name>SharePage</name>
+    <message>
+        <location filename="../share-page.cpp" line="50"/>
+        <source>Share folder</source>
+        <translation>Sdílet složku</translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="52"/>
+        <source>Share:</source>
+        <translation>Zdroj:</translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="58"/>
+        <source>Read Only</source>
+        <translation>Pouze pro čtení</translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="61"/>
+        <source>Allow Anonymous</source>
+        <translation>Umožnit anonymní</translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="73"/>
+        <source>Comment:</source>
+        <translation>Komentář:</translation>
+    </message>
+</context>
+</TS>
