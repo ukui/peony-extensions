6ae407af905d54d27a44cf28a19a4f2f0168b8cb
p-bo <pavel.borecki@gmail.com>
2020-06-02 03:28:40 +0200

    Create peony-qt-extension-computer-view-cs.ts (#21)
    Add Czech Localization

---
+++ peony-extensions-qmake/peony-qt-extension-computer-view/peony-qt-extension-computer-view-cs.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs" sourcelanguage="en_GB">
+<context>
+    <name>ComputerItemDelegate</name>
+    <message>
+        <location filename="computer-view/computer-item-delegate.cpp" line="122"/>
+        <source>You should mount volume first</source>
+        <translation>Svazek je třeba nejprve připojit (mount)</translation>
+    </message>
+</context>
+<context>
+    <name>ComputerNetworkItem</name>
+    <message>
+        <location filename="computer-view/computer-network-item.cpp" line="45"/>
+        <source>Network Neighborhood</source>
+        <translation>Okolní počítače</translation>
+    </message>
+</context>
+<context>
+    <name>ComputerRemoteVolumeItem</name>
+    <message>
+        <location filename="computer-view/computer-remote-volume-item.cpp" line="47"/>
+        <source>Remote</source>
+        <translation>Vzdálené</translation>
+    </message>
+</context>
+<context>
+    <name>ComputerVolumeItem</name>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="32"/>
+        <source>Volume</source>
+        <translation>Svazek</translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="40"/>
+        <source>File System</source>
+        <translation>Souborový systém</translation>
+    </message>
+</context>
+<context>
+    <name>Peony::ComputerViewContainer</name>
+    <message>
+        <location filename="computer-view-container.cpp" line="65"/>
+        <source>Connect a server</source>
+        <translation>Připojit k serveru</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="68"/>
+        <source>sftp://, etc...</source>
+        <translation>sftp://, atd…</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="77"/>
+        <source>Unmount</source>
+        <translation>Odpojit</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="83"/>
+        <location filename="computer-view-container.cpp" line="97"/>
+        <source>Property</source>
+        <translation>Vlastnost</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="85"/>
+        <source>You have to mount this volume first</source>
+        <translation>Tento svazek je třeba nejprve připojit (mount)</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="peony-computer-view-plugin.h" line="42"/>
+        <source>Computer View</source>
+        <translation>Tento počítač</translation>
+    </message>
+    <message>
+        <location filename="peony-computer-view-plugin.h" line="43"/>
+        <source>Show drives, network and personal directories.</source>
+        <translation>Zobrazit disky, síť a osobní složky.</translation>
+    </message>
+</context>
+</TS>
