5df8c5be35d1c130ea305bb5a536563cce93bf5e
Aylinux <cihanalk@gmail.com>
2020-06-23 21:53:45 +0300

    Create peony-qt-set-wallpaper-extension_tr.ts

---
+++ peony-extensions-qmake/peony-qt-set-wallpaper/translations/peony-qt-set-wallpaper-extension_tr.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>Peony::SetWallPaperPlugin</name>
+    <message>
+        <location filename="../set-wallpaper-plugin.h" line="42"/>
+        <source>Peony-Qt set wallpaper Extension</source>
+        <translation>Peony-Qt duvar kağıdı ayarlama eklentisi</translation>
+    </message>
+    <message>
+        <location filename="../set-wallpaper-plugin.h" line="43"/>
+        <source>set wallpaper Extension.</source>
+        <translation>Duvarkağıdı eklentisi ayarla.</translation>
+    </message>
+    <message>
+        <location filename="../set-wallpaper-plugin.cpp" line="55"/>
+        <source>Set as wallpaper</source>
+        <translation>Duvar kağıdı olarak ayarla</translation>
+    </message>
+</context>
+</TS>
