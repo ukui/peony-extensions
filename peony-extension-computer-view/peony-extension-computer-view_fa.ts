9717b4d2c26f003b3dbc4088999daa3a28da1992
Weblate <noreply@weblate.org>
2020-12-09 19:10:11 +0000

    Added translation using Weblate (Persian)

---
+++ peony-extension-computer-view/peony-extension-computer-view_fa.ts
@@ -0,0 +1,226 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fa" sourcelanguage="en_GB">
+<context>
+    <name>ComputerItemDelegate</name>
+    <message>
+        <location filename="computer-view/computer-item-delegate.cpp" line="146"/>
+        <source>You should mount volume first</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ComputerNetworkItem</name>
+    <message>
+        <location filename="computer-view/computer-network-item.cpp" line="45"/>
+        <source>Network Neighborhood</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ComputerRemoteVolumeItem</name>
+    <message>
+        <location filename="computer-view/computer-remote-volume-item.cpp" line="54"/>
+        <source>Remote</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ComputerVolumeItem</name>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="34"/>
+        <source>Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="42"/>
+        <source>File System</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoginRemoteFilesystem</name>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="41"/>
+        <source>Connect to Sever</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="89"/>
+        <source>server information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="111"/>
+        <source>user information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="141"/>
+        <source>tag</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="196"/>
+        <source>user</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="252"/>
+        <source>password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="308"/>
+        <source>protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="351"/>
+        <source>server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="382"/>
+        <source>directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="411"/>
+        <source>SAMBA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="416"/>
+        <source>FTP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="445"/>
+        <source>/</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="476"/>
+        <source>port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="508"/>
+        <source>20</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="513"/>
+        <source>21</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="518"/>
+        <source>137</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="523"/>
+        <source>138</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="528"/>
+        <source>139</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="533"/>
+        <source>445</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="592"/>
+        <source>ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="login-remote-filesystem.ui" line="623"/>
+        <source>cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Peony::ComputerViewContainer</name>
+    <message>
+        <location filename="computer-view-container.cpp" line="94"/>
+        <source>Connect a server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sftp://, etc...</source>
+        <translation type="vanished">sftp://, atd…</translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="117"/>
+        <source>Unmount</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="123"/>
+        <source>Eject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="133"/>
+        <location filename="computer-view-container.cpp" line="148"/>
+        <source>Property</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view-container.cpp" line="135"/>
+        <source>You have to mount this volume first</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="peony-computer-view-plugin.h" line="42"/>
+        <source>Computer View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="peony-computer-view-plugin.h" line="43"/>
+        <source>Show drives, network and personal directories.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="338"/>
+        <source>One or more programs prevented the unmount operation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="340"/>
+        <source>Unmount failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="341"/>
+        <source>Error: %1
+Do you want to unmount forcely?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="360"/>
+        <source>Eject failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="361"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="computer-view/computer-volume-item.cpp" line="362"/>
+        <source>Eject Anyway</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
