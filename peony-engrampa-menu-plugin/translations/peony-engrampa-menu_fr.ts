0464a55fe2e96c7806ccf95e193eca117fd5127c
Weblate <noreply@weblate.org>
2020-12-12 14:59:26 +0000

    Added translation using Weblate (French)

---
+++ peony-engrampa-menu-plugin/translations/peony-engrampa-menu_fr.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>Peony::EngrampaMenuPlugin</name>
+    <message>
+        <location filename="../engrampa-menu-plugin.cpp" line="60"/>
+        <source>compress...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.cpp" line="75"/>
+        <source>uncompress to current path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.cpp" line="85"/>
+        <source>uncompress to specific path...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>uncompress to specific path</source>
+        <translation type="vanished">rozbalit do zadaného umístění</translation>
+    </message>
+    <message>
+        <source>Peony-Qt KArchive Menu Extension</source>
+        <translation type="vanished">Rozšíření do Peony-Qt pro nabídku KArchive.</translation>
+    </message>
+    <message>
+        <source>KArchive Menu Extension.</source>
+        <translation type="vanished">Rozšíření s nabídkou KArchive.</translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.h" line="40"/>
+        <source>Peony-Qt engrampa Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../engrampa-menu-plugin.h" line="41"/>
+        <source>engrampa Menu Extension.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
