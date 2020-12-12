c6fb9aa68f7f7010949dc221ed13eaff89b1fd16
Weblate <noreply@weblate.org>
2020-12-12 14:59:26 +0000

    Added translation using Weblate (French)

---
+++ peony-share/translations/peony-share-extension_fr.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>NetUsershareHelper</name>
+    <message>
+        <source>Peony-Qt-Share-Extension</source>
+        <translation type="vanished">Peony-Qt-Share-Extension</translation>
+    </message>
+</context>
+<context>
+    <name>Peony::SharePropertiesPagePlugin</name>
+    <message>
+        <location filename="../share-properties-page-plugin.h" line="26"/>
+        <source>Peony Qt Share Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../share-properties-page-plugin.h" line="27"/>
+        <source>Allow user share folders</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SharePage</name>
+    <message>
+        <location filename="../share-page.cpp" line="50"/>
+        <source>Share folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="52"/>
+        <source>Share:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="58"/>
+        <source>Read Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="61"/>
+        <source>Allow Anonymous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../share-page.cpp" line="73"/>
+        <source>Comment:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
