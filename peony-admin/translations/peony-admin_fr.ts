e6a960cc8fe00afae8988b65a02daac0f2e53c59
roxfr <roxfr@outlook.fr>
2020-12-12 17:06:39 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: peony/extension-admin
    Translate-URL: https://weblate.ubuntukylin.com/projects/peony/extension-admin/fr/

--- peony-admin/translations/peony-admin_fr.ts
+++ peony-admin/translations/peony-admin_fr.ts
@@ -7,12 +7,12 @@
         <location filename="../admin-menu-plugin.cpp" line="22"/>
         <location filename="../admin-menu-plugin.cpp" line="37"/>
         <source>Open Directory as Admin</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir le répertoire en tant qu&apos;administrateur</translation>
     </message>
     <message>
         <location filename="../admin-menu-plugin.cpp" line="51"/>
         <source>Open Text as Admin</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir le texte en tant qu&apos;administrateur</translation>
     </message>
 </context>
 </TS>
