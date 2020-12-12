7b471c5610a8f9a932e7712496f23be9b47287fd
roxfr <roxfr@outlook.fr>
2020-12-12 18:39:51 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: peony/extension-engrampa-menu-plugin
    Translate-URL: https://weblate.ubuntukylin.com/projects/peony/extension-engrampa-menu-plugin/fr/

--- peony-engrampa-menu-plugin/translations/peony-engrampa-menu_fr.ts
+++ peony-engrampa-menu-plugin/translations/peony-engrampa-menu_fr.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../engrampa-menu-plugin.cpp" line="60"/>
         <source>compress...</source>
-        <translation type="unfinished"></translation>
+        <translation>compresser...</translation>
     </message>
     <message>
         <location filename="../engrampa-menu-plugin.cpp" line="75"/>
         <source>uncompress to current path</source>
-        <translation type="unfinished"></translation>
+        <translation>décompresser vers le chemin actuel</translation>
     </message>
     <message>
         <location filename="../engrampa-menu-plugin.cpp" line="85"/>
         <source>uncompress to specific path...</source>
-        <translation type="unfinished"></translation>
+        <translation>décompresser vers un chemin spécifique...</translation>
     </message>
     <message>
         <source>uncompress to specific path</source>
@@ -33,12 +33,12 @@
     <message>
         <location filename="../engrampa-menu-plugin.h" line="40"/>
         <source>Peony-Qt engrampa Extension</source>
-        <translation type="unfinished"></translation>
+        <translation>Extension d&apos;engrampa Peony-Qt</translation>
     </message>
     <message>
         <location filename="../engrampa-menu-plugin.h" line="41"/>
         <source>engrampa Menu Extension.</source>
-        <translation type="unfinished"></translation>
+        <translation>Extension du menu d&apos;engrampa.</translation>
     </message>
 </context>
 </TS>
