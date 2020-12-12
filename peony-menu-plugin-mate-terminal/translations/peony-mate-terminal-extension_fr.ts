03102268069e53988d1df06851c28a37912277a2
Weblate <noreply@weblate.org>
2020-12-12 14:59:26 +0000

    Added translation using Weblate (French)

---
+++ peony-menu-plugin-mate-terminal/translations/peony-mate-terminal-extension_fr.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>Peony::MateTerminalMenuPlugin</name>
+    <message>
+        <location filename="../mate-terminal-menu-plugin.cpp" line="116"/>
+        <location filename="../mate-terminal-menu-plugin.cpp" line="126"/>
+        <source>Open Directory in Terminal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mate-terminal-menu-plugin.h" line="43"/>
+        <source>Peony-Qt Mate Terminal Menu Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mate-terminal-menu-plugin.h" line="44"/>
+        <source>Open Terminal with menu.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
