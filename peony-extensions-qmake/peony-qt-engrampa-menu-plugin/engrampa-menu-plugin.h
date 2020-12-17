0cd35f624c09b681e9e847f92d045daa306873d1
MeganHemh <919052316@qq.com>
2020-12-17 09:09:37 +0800

    Fix iso file can not be uncompressed issue in right menu. Link:http://172.17.66.192/biz/bug-view-28951.html

--- peony-extensions-qmake/peony-qt-engrampa-menu-plugin/engrampa-menu-plugin.h
+++ peony-extensions-qmake/peony-qt-engrampa-menu-plugin/engrampa-menu-plugin.h
@@ -52,7 +52,7 @@ private:
     bool m_enable;
     QStringList m_file_type_list = {"7z","ar","bz","bz2","cbz","deb", "ear","exe","gz","jar","tar","tar.7z",
                                     "tar.bz2","tar.gz","tar.lzma","rar","tar.xz","war","zip","lzma","xz","tgz",
-                                    "odp", "ods", "otp", "ott", "ots", "odt"};
+                                    "odp", "ods", "otp", "ott", "ots", "odt", "iso"};
 };
 
 }
