ab3ff69c7576797601cc4a01244e89bb599bc39a
Hewenfei <xtchxf@139.com>
2021-01-27 17:10:39 +0800

    rebuild properties window #sharePage 新增: 1.重构ui界面。 2.响应确认按钮。

--- peony-share/share-page.cpp
+++ peony-share/share-page.cpp
@@ -22,136 +22,221 @@
 
 #include "share-page.h"
 #include "share-properties-page-plugin.h"
-#include <QUrl>
 
+#include <PeonyFileInfoJob>
+#include <QUrl>
 #include <QFormLayout>
 #include <QPushButton>
 #include <QLabel>
 #include <QCheckBox>
 #include <QButtonGroup>
 #include <PeonyFileInfo>
-
-#include <QTextEdit>
+#include <QtConcurrent>
 
 #include <QDebug>
 
 SharePage::SharePage(const QString &uri, QWidget *parent) : PropertiesWindowTabIface(parent)
 {
-    QUrl url = uri;
-    m_share_info = ShareInfo(url.fileName(), false);
-    m_share_info.originalPath = url.path();
+    QFuture<void> initFileInfoJob = QtConcurrent::run([=]() {
+        m_fileInfo = Peony::FileInfo::fromUri(uri);
+        Peony::FileInfoJob *fileInfoJob = new Peony::FileInfoJob(m_fileInfo);
+        fileInfoJob->setAutoDelete(true);
+        fileInfoJob->querySync();
+    });
+
+    m_theFutureWatcher = new QFutureWatcher<void>();
+    m_theFutureWatcher->setFuture(initFileInfoJob);
+
+    connect(m_theFutureWatcher, &QFutureWatcher<void>::finished, this, &SharePage::init);
+}
+
+void SharePage::init()
+{
+    if (m_theFutureWatcher)
+        delete m_theFutureWatcher;
+
+    m_shareInfo = ShareInfo(m_fileInfo->displayName(), false);
+    m_shareInfo.originalPath = m_fileInfo->filePath();
     auto manager = Peony::SharePropertiesPagePlugin::getInstance();
     auto infos = manager->getCurrentShareInfos();
     for (auto info : infos) {
-        qDebug()<<"compare"<<m_share_info.name<<info.name<<m_share_info.originalPath<<info.originalPath;
-        if (m_share_info.name == info.name && info.originalPath == info.originalPath) {
-            m_share_info.comment = info.comment;
-            m_share_info.readOnly = info.readOnly;
-            m_share_info.allowGuest = info.allowGuest;
-            m_share_info.originalPath = info.originalPath;
-            m_share_info.isShared = true;
+        qDebug() << "compare" << m_shareInfo.name << info.name << m_shareInfo.originalPath << info.originalPath;
+        if (m_shareInfo.name == info.name && m_shareInfo.originalPath == info.originalPath) {
+            m_shareInfo.comment = info.comment;
+            m_shareInfo.readOnly = info.readOnly;
+            m_shareInfo.allowGuest = info.allowGuest;
+            m_shareInfo.originalPath = info.originalPath;
+            m_shareInfo.isShared = true;
             break;
         }
     }
 
-    auto layout = new QFormLayout(this);
-    auto info = Peony::FileInfo::fromUri(uri);
-    QPushButton *bt = new QPushButton(this);
-    bt->setIcon(QIcon::fromTheme(info->iconName()));
-    bt->setIconSize(QSize(48, 48));
-    QLabel *uriLabel = new QLabel(url.path(), this);
-    uriLabel->setTextInteractionFlags(Qt::TextSelectableByMouse);
-    layout->addRow(bt, uriLabel);
+    m_layout = new QVBoxLayout(this);
+    m_layout->setMargin(0);
 
-    QFrame *s = new QFrame(this);
-    s->setFrameShape(QFrame::HLine);
-    layout->addRow(s, s);
+    this->initFloorOne();
 
-    bool shared = m_share_info.isShared;
+    this->addSeparate();
 
-    QCheckBox *shareCheckBox = new QCheckBox(tr("Share folder"), this);
-    shareCheckBox->setChecked(m_share_info.isShared);
-    layout->addRow(tr("Share:"), shareCheckBox);
+    this->initFloorTwo();
 
-    s = new QFrame(this);
-    s->setFrameShape(QFrame::HLine);
-    layout->addWidget(s);
+    this->initFloorThree();
 
-    QCheckBox *shareReadOnlyCheckBox = new QCheckBox(tr("Read Only"), this);
-    shareReadOnlyCheckBox->setChecked(m_share_info.readOnly);
-
-    QCheckBox *shareAllowGuestCheckBox = new QCheckBox(tr("Allow Anonymous"));
-    shareAllowGuestCheckBox->setChecked(m_share_info.allowGuest);
-
-    layout->addWidget(shareReadOnlyCheckBox);
-    layout->addWidget(shareAllowGuestCheckBox);
-
-    s->setVisible(shared);
-    shareReadOnlyCheckBox->setVisible(shared);
-    shareAllowGuestCheckBox->setVisible(shared);
-
-    QTextEdit *edit = new QTextEdit(this);
-    edit->setText(m_share_info.comment);
-    QLabel *comment = new QLabel(tr("Comment:"), this);
-    layout->addRow(comment, edit);
-
-    comment->setVisible(shared);
-    edit->setVisible(shared);
-
-    connect(edit, &QTextEdit::textChanged, this, [=](){
-        m_share_info.comment = edit->toPlainText();
-        //FIXME: should i update share info (exec cmd) so frequently?
-        NetUsershareHelper::updateShareInfo(m_share_info);
-    });
-
-    connect(shareCheckBox, &QCheckBox::clicked, this, [=](bool checked){
-        if (checked) {
-            m_share_info.name = url.fileName();
-            m_share_info.readOnly = shareReadOnlyCheckBox->isChecked();
-            m_share_info.allowGuest = shareAllowGuestCheckBox->isChecked();
-
-            NetUsershareHelper::updateShareInfo(m_share_info);
-
-            shareCheckBox->setChecked(m_share_info.isShared);
-            shareReadOnlyCheckBox->setVisible(m_share_info.isShared);
-            shareAllowGuestCheckBox->setVisible(m_share_info.isShared);
-            s->setVisible(m_share_info.isShared);
-            comment->setVisible(m_share_info.isShared);
-            edit->setVisible(m_share_info.isShared);
-
-            if (m_share_info.isShared) {
-                /*!
-                  \todo
-                  add emblem info to peony-qt's tags(emblems) & cloumns extensions frameworks.
-                  before do it, i have to implement that frameworks first...
-                  */
-            }
-        } else {
-            s->setVisible(false);
-            shareReadOnlyCheckBox->setVisible(false);
-            shareAllowGuestCheckBox->setVisible(false);
-            comment->setVisible(false);
-            edit->setVisible(false);
-
-            NetUsershareHelper::removeShared(m_share_info.name);
-            m_share_info.isShared = false;
-        }
-    });
-
-    connect(shareReadOnlyCheckBox, &QCheckBox::clicked, this, [=](bool checked){
-        m_share_info.readOnly = checked;
-        NetUsershareHelper::updateShareInfo(m_share_info);
-    });
-
-    connect(shareAllowGuestCheckBox, &QCheckBox::clicked, this, [=](bool checked){
-        m_share_info.allowGuest = checked;
-        NetUsershareHelper::updateShareInfo(m_share_info);
-    });
+    m_layout->addStretch(1);
 }
 
 void SharePage::saveAllChange()
 {
-    qDebug() << "共享页面，保存全部函数";
-    if (!this->m_thisPageChanged)
+    qDebug() << __FILE__ << __FUNCTION__ << "page changed?" << m_thisPageChanged;
+
+    if (!m_thisPageChanged)
         return;
+
+    bool checked = m_shareCheckBox->isChecked();
+
+    if (checked) {
+        //FIXME:暂时不支持自定义共享名 - Currently does not support custom shared name
+        m_shareInfo.name = m_fileInfo->displayName();
+        m_shareInfo.readOnly = m_shareReadOnlyCheckBox->isChecked();
+        m_shareInfo.allowGuest = m_shareAllowGuestCheckBox->isChecked();
+        m_shareInfo.isShared = true;
+
+        NetUsershareHelper::updateShareInfo(m_shareInfo);
+    } else {
+        //FIXME:需要先判断存在共享吗 - Need to determine whether there is sharing first
+        NetUsershareHelper::removeShared(m_shareInfo.name);
+    }
+}
+
+void SharePage::initFloorOne()
+{
+    QFrame * floor1 = new QFrame(this);
+    floor1->setMinimumHeight(90);
+
+    QHBoxLayout * layout1 = new QHBoxLayout(floor1);
+    layout1->setContentsMargins(22, 0, 22, 0);
+    layout1->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    floor1->setLayout(layout1);
+
+    m_iconButton = new QPushButton(floor1);
+    m_iconButton->setIcon(QIcon::fromTheme(m_fileInfo.get()->iconName()));
+    m_iconButton->setIconSize(QSize(48, 48));
+
+    layout1->addWidget(m_iconButton);
+
+    QVBoxLayout * vBoxLayout = new QVBoxLayout(floor1);
+
+    m_folderName = new QLabel(floor1);
+    m_sharedState = new QLabel(floor1);
+
+    m_folderName->setText(m_fileInfo.get()->displayName());
+    m_sharedState->setText(m_shareInfo.isShared ? tr("share this folder") : tr("don`t share this folder"));
+
+    vBoxLayout->addStretch(1);
+    vBoxLayout->addWidget(m_folderName);
+    vBoxLayout->addWidget(m_sharedState);
+    vBoxLayout->addStretch(1);
+
+    layout1->addLayout(vBoxLayout);
+    layout1->addStretch(1);
+
+    m_layout->addWidget(floor1);
+}
+
+void SharePage::initFloorTwo()
+{
+    QFrame * floor2 = new QFrame(this);
+    floor2->setMinimumHeight(64);
+
+    QVBoxLayout * layout2 = new QVBoxLayout(floor2);
+    layout2->setContentsMargins(22, 0, 22, 0);
+
+    floor2->setLayout(layout2);
+
+    bool shared = m_shareInfo.isShared;
+
+    m_shareCheckBox = new QCheckBox(tr("Share folder"), floor2);
+    m_shareCheckBox->setChecked(shared);
+
+    layout2->addWidget(m_shareCheckBox);
+
+    m_layout->addWidget(floor2);
+
+    connect(m_shareCheckBox, &QCheckBox::clicked, this, [=](bool checked) {
+        this->thisPageChanged();
+        if (checked) {
+            //FIXME:暂时不支持自定义共享名 - Currently does not support custom shared name
+            m_shareInfo.name = m_fileInfo->displayName();
+            m_shareInfo.readOnly = m_shareReadOnlyCheckBox->isChecked();
+            m_shareInfo.allowGuest = m_shareAllowGuestCheckBox->isChecked();
+
+            if (m_floor3)
+                m_floor3->setVisible(true);
+
+            m_sharedState->setText(tr("share this folder"));
+            m_shareInfo.isShared = true;
+
+        } else {
+            if (m_floor3)
+                m_floor3->setVisible(false);
+
+            m_sharedState->setText(tr("don`t share this folder"));
+            m_shareInfo.isShared = false;
+        }
+    });
+}
+
+void SharePage::initFloorThree()
+{
+    m_floor3 = new QFrame(this);
+
+    QVBoxLayout * layout3 = new QVBoxLayout(m_floor3);
+    layout3->setContentsMargins(22, 0, 22, 0);
+
+    m_floor3->setLayout(layout3);
+
+    QFormLayout *formLayout = new QFormLayout(m_floor3);
+
+    m_shareNameEdit = new QLineEdit(m_floor3);
+    m_shareNameEdit->setMaxLength(32);
+    m_shareNameEdit->setEnabled(false);
+    m_shareNameEdit->setText(m_shareInfo.name);
+
+    m_commentEdit = new QLineEdit(m_floor3);
+    m_commentEdit->setMaxLength(64);
+    m_commentEdit->setText(m_shareInfo.comment);
+
+    formLayout->addRow(tr("Share name"), m_shareNameEdit);
+    formLayout->addRow(tr("Comment"), m_commentEdit);
+
+    layout3->addLayout(formLayout);
+
+    m_shareReadOnlyCheckBox = new QCheckBox(tr("Read Only"), this);
+    m_shareReadOnlyCheckBox->setChecked(m_shareInfo.readOnly);
+
+    m_shareAllowGuestCheckBox = new QCheckBox(tr("Allow Anonymous"));
+    m_shareAllowGuestCheckBox->setChecked(m_shareInfo.allowGuest);
+
+    layout3->addWidget(m_shareReadOnlyCheckBox);
+    layout3->addWidget(m_shareAllowGuestCheckBox);
+
+    m_floor3->setVisible(m_shareInfo.isShared);
+
+    m_layout->addWidget(m_floor3);
+
+    connect(m_commentEdit, &QLineEdit::textChanged, this, [=]() {
+        m_shareInfo.comment = m_commentEdit->text();
+        this->thisPageChanged();
+    });
+
+    connect(m_shareReadOnlyCheckBox, &QCheckBox::clicked, this, [=](bool checked) {
+        m_shareInfo.readOnly = checked;
+        this->thisPageChanged();
+    });
+
+    connect(m_shareAllowGuestCheckBox, &QCheckBox::clicked, this, [=](bool checked) {
+        m_shareInfo.allowGuest = checked;
+        this->thisPageChanged();
+    });
 }
