f1719c6b50e9f45205e70a20a46f152cb4a1b06f
dingjingmaster <dingjing@live.cn>
2021-03-11 20:39:00 +0800

    [ADD]  send file to device

---
+++ peony-send-to-device/translations/peony-send-to-device_zh_CN.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Peony::SendToPlugin</name>
+    <message>
+        <location filename="../send-to-plugin.cpp" line="106"/>
+        <source>Send to a removable device</source>
+        <translation>发送到移动设备</translation>
+    </message>
+</context>
+</TS>
