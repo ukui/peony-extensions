808d82cb6d517225c551a185b73e1a8f429ec6b0
dingjingmaster <dingjing@live.cn>
2021-06-30 13:49:19 +0800

    [FIX]  屏蔽只读U盘 [LINK] 57473

--- peony-send-to-device/send-to-device-plugin.cpp
+++ peony-send-to-device/send-to-device-plugin.cpp
@@ -34,6 +34,7 @@
 #include <QApplication>
 #include <file-copy-operation.h>
 #include <file-operation-manager.h>
+#include <file-info-job.h>
 
 static QString getIconName (GIcon* icons);
 static void mounted_func (gpointer data, gpointer udata);
@@ -142,7 +143,7 @@ static void mounted_func (gpointer data, gpointer udata)
     GIcon*              icons = nullptr;
     QString             icon = nullptr;
 
-    if (!data || !udata) return;
+    if (!data || !udata || !static_cast<DriverAction*>(udata)) return;
 
     if (mount) {
         name = g_mount_get_name(mount);
@@ -153,10 +154,15 @@ static void mounted_func (gpointer data, gpointer udata)
         }
     }
 
+    // check permission
+    FileInfoJob fileInfo(uri);
+    fileInfo.querySync();
+    std::shared_ptr<FileInfo> info = fileInfo.getInfo();
+
     icon = getIconName (icons);
 
-    if (uri && name) {
-        ((DriverAction*)udata)->driverAdded(uri, name, icon);
+    if (uri && name && info.get()->canExecute() && info.get()->canWrite()) {
+        (static_cast<DriverAction*>(udata))->driverAdded(uri, name, icon);
     }
 
     qDebug() << "name:" << name << " uri:" << uri << " icons:" << icons << " icon:" << icon;
@@ -259,6 +265,7 @@ DriverAction::DriverAction(const QStringList& uris, QObject *parent) : QAction(p
 
     setMenu(mMenu);
     setText(tr("Send to a removable device"));
+    showAction();
 }
 
 DriverAction::~DriverAction()
