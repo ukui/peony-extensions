f219b5ef09bd30464c6d4e68e383a404ba0a58cb
dingjing <dingjing@live.cn>
2021-11-05 14:52:19 +0800

    添加弹框

--- peony-drive-rename/translations/peony-drive-rename_tr.ts
+++ peony-drive-rename/translations/peony-drive-rename_tr.ts
@@ -9,24 +9,30 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../drive-rename.cpp" line="66"/>
-        <location filename="../drive-rename.cpp" line="78"/>
+        <location filename="../drive-rename.cpp" line="122"/>
+        <location filename="../drive-rename.cpp" line="134"/>
         <source>Rename</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../drive-rename.cpp" line="78"/>
+        <location filename="../drive-rename.cpp" line="134"/>
         <source>Device name:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../drive-rename.cpp" line="83"/>
+        <location filename="../drive-rename.cpp" line="138"/>
+        <location filename="../drive-rename.cpp" line="150"/>
         <source>Warning</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../drive-rename.cpp" line="83"/>
-        <source>Failed to rename!</source>
+        <location filename="../drive-rename.cpp" line="138"/>
+        <source>Renaming will unmount the device. Do you want to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../drive-rename.cpp" line="150"/>
+        <source>The device may not support the rename operation, rename failed!</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
